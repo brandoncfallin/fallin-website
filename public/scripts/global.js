c7c67f7a4abd238c9fa90475b8b6c26b92f6952c
Brandon Fallin <brandonfallin@gmail.com>
2023-06-21 14:39:03 -0400

    edits

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,4 +1,12 @@
 function darkModeToggle() {
     var element = document.body;
     element.classList.toggle("dark");
+
+    var change = document.getElementById("dark-mode-toggle");
+    if (change.innerHTML === "Dark Mode") {
+        change.innerHTML = "Light Mode";
+    }
+    else {
+        change.innerHTML = "Dark Mode";
+    }
   } 
