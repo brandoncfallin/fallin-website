dc80fc2038d90059e360742988a07cb715d4726f
trophyguarder <brandonfallin@gmail.com>
2023-11-21 18:42:38 -0500

    trying again

--- resources/js/dark.js
+++ resources/js/dark.js
@@ -10,23 +10,3 @@ if (localStorage.getItem("dark-mode")) {
 else {
   localStorage.setItem("dark-mode", "light");
 }
-
-if (localStorage.getItem("dark-mode") == "dark") {
-  document.getElementById("nav-icon").src = "data/icons/menu-wht.svg";
-  
-  document.body.classList.remove("animated");
-  document.body.classList.add("dark");
-  document.getElementById("nav-menu").classList.add("dark");
-  
-  document.getElementById("dark-mode-toggle").src = "data/icons/sun.svg";
-  document.getElementById("dark-mode-toggle-mobile").src = "data/icons/sun.svg";
-}
-if (localStorage.getItem("dark-mode") == "light") {
-  document.body.classList.remove("animated");
-  document.body.classList.remove("dark");
-  document.getElementById("nav-menu").classList.remove("dark");
-  
-  document.getElementById("dark-mode-toggle").src = "data/icons/moon.svg";
-  document.getElementById("dark-mode-toggle-mobile").src = "data/icons/moon.svg";
-  document.getElementById("nav-icon").src = "data/icons/menu-blk.svg";
-}
