b78c79d4b8c565acefdb1870e56f7d01f3cd55cf
brandoncfallin <brandonfallin@gmail.com>
2024-10-19 12:32:25 -0400

    try this

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,11 +1,11 @@
-// Waits for page to load
-document.addEventListener("DOMContentLoaded",() => { 
-  // Check local storage for dark mode. Gets rid of white flash on load 
-  if (sessionStorage.getItem("dark-mode") == "dark") {
-    makeDark();
-  }
-  else {
-    makeLight();
-  }
+// Check local storage for dark mode immediately
+if (sessionStorage.getItem("dark-mode") == "dark") {
+  makeDark();
+} else {
+  makeLight();
+}
+
+// Wait for DOM to be fully loaded before removing 'hidden' class
+document.addEventListener("DOMContentLoaded", () => {
   document.documentElement.classList.remove("hidden");
 });
