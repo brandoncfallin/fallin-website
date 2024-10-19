824a8009be9d9fe030c7e62b2a8130c763670375
brandoncfallin <brandonfallin@gmail.com>
2024-10-19 13:07:50 -0400

    get rid of transform scale

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,11 +1,10 @@
-// Check local storage for dark mode immediately
-if (sessionStorage.getItem("dark-mode") == "dark") {
-  makeDark();
-} else {
-  makeLight();
-}
-
 // Wait for DOM to be fully loaded before removing 'hidden' class
 document.addEventListener("DOMContentLoaded", () => {
+  // Check local storage for dark mode immediately
+  if (sessionStorage.getItem("dark-mode") == "dark") {
+    makeDark();
+  } else {
+    makeLight();
+  }
   document.documentElement.classList.remove("hidden");
 });
